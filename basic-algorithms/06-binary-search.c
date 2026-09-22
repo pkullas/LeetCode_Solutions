5fafbad2ce75bd29bf4536d6c8d27c76acf4b3d6
JeevanKumars890 <jeevankumars8904@gmail.com>
2026-09-22 23:13:27 +0530

    Add Binary Search solution, documentation and screenshot

---
+++ basic-algorithms/06-binary-search.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int binarySearch(int nums[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    // Test Case 1
+    int nums1[] = {-1, 0, 3, 5, 9, 12};
+    int target1 = 9;
+
+    printf("Test Case 1: %d\n", binarySearch(nums1, 6, target1));
+
+    // Test Case 2 - Edge Case
+    int nums2[] = {-1, 0, 3, 5, 9, 12};
+    int target2 = 2;
+
+    printf("Test Case 2: %d\n", binarySearch(nums2, 6, target2));
+
+    return 0;
+}
